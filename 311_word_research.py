0a3c1c87fa54e08be0231be6fc34f9a5a68695e1
michael vogelsang <mjvoge02@louisville.edu>
2017-02-25 04:55:07 -0500

    word analysis

--- 311_word_research.py
+++ 311_word_research.py
@@ -10,10 +10,48 @@ def main():
     #load csv of 311
     dat = open("./Data/Citizen311Data_STD.csv")
     csvDat = pd.read_csv(dat)
-    complaintText = df.description
-    print(complaintText[1])
+    dat.close()
+    complaintText = csvDat.description
+
+    #everything already in all caps
+    #so, remove all punctuation
+    purifiedText = []
+    punctuation = ["\\", "," , ".", "'", "/", "(", ")", "-", "&"]
+    for item in complaintText:
+        temp = item
+        for punc in punctuation:
+            temp=temp.replace(punc, "")
+        purifiedText.append(temp)
 
 
+    #get all the text in a string
+    fullText = ""
+    for line in purifiedText:
+        fullText += " " + line
+
+    #delete huge useless stuff
+    del(purifiedText)
+    del(complaintText)
+
+    #get unique tokens
+    tokens = fullText.split()
+    uniqTok = set(tokens)
+    uniqueTokens = list(uniqTok)
+    del(uniqTok)
+    uniqueTokens.sort()
+
+    #count up occurrences
+    utokdict = {}
+    for u in uniqueTokens:
+        utokdict[u] = 0
+
+    for t in tokens:
+        utokdict[t] +=1
+
+    output = open("./Data/311_keyword_results.txt", "w+")
+    for u in uniqueTokens:
+        output.write(u + " " + str(utokdict[u]) + "\n")
+    output.close()
 
 if __name__ == "__main__":
     main()
