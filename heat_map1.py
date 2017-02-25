7f4555c79bcf3cc120f0847564b4ce61fd6b3d37
Jordan Dodson <jordandodson22@gmail.com>
2017-02-25 17:43:47 -0500

    working on heat_map1.py

--- heat_map1.py
+++ heat_map1.py
@@ -9,14 +9,15 @@ with open(data_file, 'r') as f:
 
 lats = []
 longs = []
-avg_points_missed = []
+weights = []
 
 # weight data points based on their score
 for d in data.values():
   if len(d['scores']) >= 5:
     lats.append(d['lat'])
     longs.append(d['long'])
-    avg_points_missed.append((100 - np.mean([s[2] for s in d['scores']]))**3)
+    w = (100 - np.mean([s[2] for s in d['scores']]))**2
+    weights.append(w)
 
 lat_res = 0.02
 long_res = 0.02
@@ -27,10 +28,10 @@ long_mean = np.mean(longs)
 lat_std = np.std(lats)
 long_std = np.std(longs)
 
-lat_limits = [lat_mean - 0.2*lat_std, lat_mean + 0.2*lat_std]
-long_limits = [long_mean - 0.2*long_std, long_mean + 0.2*long_std]
+lat_limits = [lat_mean - 0.1*lat_std, lat_mean + 0.1*lat_std]
+long_limits = [long_mean - 0.1*long_std, long_mean + 0.05*long_std]
 
-H, x_edges, y_edges = np.histogram2d(x=lats, y=longs, bins=500, range=[lat_limits, long_limits], normed=True, weights=avg_points_missed)
+H, x_edges, y_edges = np.histogram2d(x=lats, y=longs, bins=500, range=[lat_limits, long_limits], normed=True, weights=weights)
 
 X, Y = np.meshgrid(x_edges, y_edges)
 
