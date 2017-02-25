53bafaccff77e3c1e27027c337e11a18b4764121
Jordan Dodson <jordandodson22@gmail.com>
2017-02-25 14:21:55 -0500

    adding plots directory

--- lat_long_poly_regression1.py
+++ lat_long_poly_regression1.py
@@ -1,21 +1,21 @@
 import json
 import numpy as np
-from sklearn import linear_model, svm
+from sklearn import linear_model
 
-input_file = 'clean_data/combined_inspection_data.json'
+input_file = 'clean_data/grouped_louisville_inspections.json'
 
-# Load the dataset
 with open(input_file, 'r') as f:
   data = dict(json.load(f))
 
-# Pairs of inputs and targets for regression
 pairs = []
 
 for d in data.values():
 
   # Only include restaurants with at least 3 inspections
-  if 10 <= len(d['scores']) <= 30:
-    avg_score = np.mean([score[2] for score in d['scores']])
+  if len(d['scores']) >= 3:
+    avg_score = sum([score[2] for score in d['scores']]) / len(d['scores'])
+    if isinstance(d['lat'], str) or isinstance(d['long'], str):
+      print('found a lat/long string')
     lat_long = [d['lat'], d['long']]
     pairs.append([lat_long, avg_score])
 
@@ -36,12 +36,9 @@ test_Y = [d[1] for d in test_data]
 
 # Initialize the model
 reg = linear_model.LinearRegression()
-svreg = svm.SVR()
 
 # Train the model
 reg.fit(train_X, train_Y)
-svreg.fit(train_X, train_Y)
 
 # Test the model
-print('Linear Reg: {}'.format(reg.score(test_X, test_Y)))
-print('SVG Reg: {}'.format(svreg.score(test_X, test_Y)))
+print(reg.score(test_X, test_Y))
