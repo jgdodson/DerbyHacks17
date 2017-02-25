55fd09c5219583029440672232c2e139baf1e6cc
michael vogelsang <mjvoge02@louisville.edu>
2017-02-25 02:28:23 -0500

    first crack at data

---
+++ 311_word_research.py
@@ -0,0 +1,19 @@
+import sys
+import math
+import time
+import random
+import pandas as pd
+
+
+
+def main():
+    #load csv of 311
+    dat = open("./Data/Citizen311Data_STD.csv")
+    csvDat = pd.read_csv(dat)
+    complaintText = df.description
+    print(complaintText[1])
+
+
+
+if __name__ == "__main__":
+    main()
